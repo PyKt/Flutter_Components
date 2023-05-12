344b9e9eebff52e1da2b8638145d1715d9402303
PyKt <francisco4dom@gmail.com>
2023-05-11 22:41:53 -0400

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(useMaterial3: true),
         initialRoute: AppRoutes.initialRoute,
         routes: AppRoutes.getAppRoutes(),
         onGenerateRoute: (settings) => AppRoutes.onGenerareRoute(settings));
