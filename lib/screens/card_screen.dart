01f3c0749e0eae77ae03bf52981af939d73e0579
PyKt <francisco4dom@gmail.com>
2023-05-11 22:45:16 -0400

    corrections docker

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -11,8 +11,7 @@ class CardScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: const Center(),
-      floatingActionButtonLocation:
-          FloatingActionButtonLocation.miniStartDocked,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.pushNamed(context, "HomeScreen");
